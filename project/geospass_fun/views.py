0d64d00fc976d3d6927ccae770e0f6fa666dd414
ingrih <ingrih_@hotmail.com>
2017-11-17 12:13:21 +0000

    added frontpage to the project

--- project/geospass_fun/views.py
+++ project/geospass_fun/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, render
+
+def page(request):
+    return render(request, 'geospass_fun/index.html')
+    
+def header(request):
+    return render(request, 'geospass_fun/header.html')
--- project/project/settings.py
+++ project/project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'geospass_fun',
+    'home',
 ]
 
 MIDDLEWARE_CLASSES = [
